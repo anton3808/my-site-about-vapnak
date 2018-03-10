11d6d240fe8f6a6a69e592d6da7d54c06ff39cd6
anton3808 <antonsadlov@gmail.com>
2018-03-11 00:53:08 +0200

    Add files via upload

--- services_ru/static/services_ru/js/main.js
+++ services_ru/static/services_ru/js/main.js
@@ -99,3 +99,22 @@ $(document).ready(function(){
 		$('#english_langua a').removeClass('english_langua_a_hover');
 	});
 });
+
+
+
+
+
+
+
+
+
+
+
+
+					// Mask for number phone
+jQuery(function($){
+   // $("#date").mask("99/99/9999");
+   $("#phone").mask("+99(999) 999-9999");
+   // $("#tin").mask("99-9999999");
+   // $("#ssn").mask("999-99-9999");
+});
