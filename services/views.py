c09afddab9e334908f96d732e6b6d5974ba653a0
anton3808 <antonsadlov@gmail.com>
2018-03-11 23:08:53 +0200

    Add files via upload

--- services/views.py
+++ services/views.py
@@ -1,16 +1,19 @@
-from django.core.mail import send_mail, BadHeaderError
-from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.conf import settings
-
-from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse, HttpResponseRedirect
 
+
+
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
 # Create your views here.
+from django.core.mail import EmailMessage
+
 
 
 def send(request):
-    # return HttpResponseRedirect("production/")
     contact_person = request.POST.get('contact_person', '')
     phone = request.POST.get('phone', '')
     cargo = request.POST.get('cargo', '')
@@ -22,20 +25,42 @@ def send(request):
 
 
 
-
-    subject = "Оrganization of cargo transportation"
-    from_email = settings.EMAIL_HOST_USER
-    to_email = from_email
     contact_message = "Контактна особа: %s, номер телефона: %s, вантаж: %s, код вантажу по залізниці: %s, станція навантаження: %s, код станції навантаження: %s, станція вивантаження: %s, код станція вивантаження: %s"%(contact_person, phone, cargo, cod_cargo_railway, load_station, cod_load_station, unloading_station, cod_unloading_station)
     
+
+    from_addr = 'antonsadlov@ukr.net'
+    to_addr = 'antonsadlov@ukr.net'
+
+    username = 'antonsadlov@ukr.net'
+    password = 'antoha2003'
+
+    msg = MIMEMultipart()
+
+    msg['From'] = from_addr
+    msg['To'] = to_addr
+    msg['Subject'] = 'Organization of cargo transportation'
+    msg.attach(MIMEText(contact_message))
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.ukr.net:2525')
+    except TimeoutError:
+        return HttpResponseRedirect('/')
+
+
+    server.login(username,password)
+
     if contact_person and phone and cargo and cod_cargo_railway and load_station and cod_load_station and unloading_station and cod_unloading_station:       
         try:
-            send_mail(subject, contact_message, from_email, ['admin@triumph-vapnak.com.ua'])
+            server.sendmail(from_addr,to_addr,msg.as_string())
         except BadHeaderError:
             return HttpResponse('Invalid header found.')
+        server.quit()
         return HttpResponseRedirect('../')
     else:
         return HttpResponse('Make sure all fields are entered and valid.')
+
+
+
 
 
 
@@ -73,9 +98,7 @@ def services_search_mobile(request):
         else:
             return render(request, 'error/wrapper.html')
 
-# def form_services(request):
-#     return HttpResponseRedirect("contact/")
-    # return render(request, 'about/wrapper.html')
+
 
 
 
