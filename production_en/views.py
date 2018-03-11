23c439dc5294e8f27010a24cae0cd23917d9c57b
anton3808 <antonsadlov@gmail.com>
2018-03-11 23:08:23 +0200

    Add files via upload

--- production_en/views.py
+++ production_en/views.py
@@ -2,126 +2,211 @@ from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.conf import settings
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse, HttpResponseRedirect
 
-def send_email_4(request):
+def send_email_4_en(request):
     user_surname = request.POST.get('user_surname', '')
-    user_name = request.POST.get('user_name', '')
+    volume = request.POST.get('volume', '')
     email = request.POST.get('email', '')
     phone = request.POST.get('phone', '')
     select_fraction = request.POST.get('select_fraction', '')
     enterprise = request.POST.get('enterprise', '')
     delivery_period = request.POST.get('delivery_period', '')
     message = request.POST['message']
-    # message = request.POST.get('message', '')
 
-    subject = "Contact form(dla kombikormov i pidkormku ptizu)"
-    from_email = settings.EMAIL_HOST_USER
-    to_email = from_email
-    if message:
-    	contact_message = "Фамілія: %s, імя: %s, почта: %s, номер телефона: %s, фракція: %s, підприємство: %s, період доставки: %s, текстов повідомлення: %s"%(user_surname, user_name, email, phone, select_fraction, enterprise, delivery_period, message)
-    else:
-    	contact_message = "Фамілія: %s, імя: %s, почта: %s, номер телефона: %s, фракція: %s, підприємство: %s, період доставки: %s"%(user_surname, user_name, email, phone, select_fraction, enterprise, delivery_period)
     
-    if user_surname and user_name and email and phone and select_fraction and enterprise and delivery_period:
-        try:
-            send_mail(subject, contact_message, from_email, ['antonsadlov@gmail.com'])
-        except BadHeaderError:
-            return HttpResponse('Invalid header found.')
-        return HttpResponseRedirect('../')
-    else:
-        return HttpResponse('Make sure all fields are entered and valid.')
-
-
-def send_email_3(request):
-    user_surname = request.POST.get('user_surname', '')
-    user_name = request.POST.get('user_name', '')
-    email = request.POST.get('email', '')
-    phone = request.POST.get('phone', '')
-    select_fraction = request.POST.get('select_fraction', '')
-    enterprise = request.POST.get('enterprise', '')
-    delivery_period = request.POST.get('delivery_period', '')
-    message = request.POST['message']
-    # message = request.POST.get('message', '')
-
-    subject = "Contact form(dla zukrovoi promuslovosti)"
-    from_email = settings.EMAIL_HOST_USER
-    to_email = from_email
     if message:
-    	contact_message = "Фамілія: %s, імя: %s, почта: %s, номер телефона: %s, фракція: %s, підприємство: %s, період доставки: %s, текстов повідомлення: %s"%(user_surname, user_name, email, phone, select_fraction, enterprise, delivery_period, message)
+        contact_message = "Контактна особа: %s, почта: %s, номер телефона: %s, обєм вантажу: %s, фракція: %s, підприємство: %s, період доставки: %s, текстов повідомлення: %s"%(user_surname, email, phone, volume, select_fraction, enterprise, delivery_period, message)
     else:
-    	contact_message = "Фамілія: %s, імя: %s, почта: %s, номер телефона: %s, фракція: %s, підприємство: %s, період доставки: %s"%(user_surname, user_name, email, phone, select_fraction, enterprise, delivery_period)
-    if user_surname and user_name and email and phone and select_fraction and enterprise and delivery_period:
-        try:
-            send_mail(subject, contact_message, from_email, ['antonsadlov@gmail.com'])
-        except BadHeaderError:
-            return HttpResponse('Invalid header found.')
-        return HttpResponseRedirect('../')
-    else:
-        return HttpResponse('Make sure all fields are entered and valid.')
-
-
-
-
-def send_email_2(request):
-    user_surname = request.POST.get('user_surname', '')
-    user_name = request.POST.get('user_name', '')
-    email = request.POST.get('email', '')
-    phone = request.POST.get('phone', '')
-    select_fraction = request.POST.get('select_fraction', '')
-    enterprise = request.POST.get('enterprise', '')
-    delivery_period = request.POST.get('delivery_period', '')
-    message = request.POST['message']
-    # message = request.POST.get('message', '')
-
-    subject = "Contact form(dla rozkusnenya gruntu i vodoyom)"
-    from_email = settings.EMAIL_HOST_USER
-    to_email = from_email
-    if message:
-    	contact_message = "Фамілія: %s, імя: %s, почта: %s, номер телефона: %s, фракція: %s, підприємство: %s, період доставки: %s, текстов повідомлення: %s"%(user_surname, user_name, email, phone, select_fraction, enterprise, delivery_period, message)
-    else:
-    	contact_message = "Фамілія: %s, імя: %s, почта: %s, номер телефона: %s, фракція: %s, підприємство: %s, період доставки: %s"%(user_surname, user_name, email, phone, select_fraction, enterprise, delivery_period)
-    if user_surname and user_name and email and phone and select_fraction and enterprise and delivery_period:
-        try:
-            send_mail(subject, contact_message, from_email, ['antonsadlov@gmail.com'])
-        except BadHeaderError:
-            return HttpResponse('Invalid header found.')
-        return HttpResponseRedirect('../')
-    else:
-        return HttpResponse('Make sure all fields are entered and valid.')
-
-
-
-
-
-
-
-def send_email_1(request):
-    contact_person = request.POST.get('contact_person', '')
-    phone = request.POST.get('phone', '')
-    cargo = request.POST.get('cargo', '')
-    cod_cargo_railway = request.POST.get('cod_cargo_railway', '')
-    load_station = request.POST.get('load_station', '')
-    cod_load_station = request.POST.get('cod_load_station', '')
-    unloading_station = request.POST.get('unloading_station_period', '')
-    cod_unloading_station = request.POST['cod_unloading_station']
-    # cod_unloading_station = request.POST['cod_unloading_station']
-
-
-
-
-    subject = "Contact form(dla metalurgii ta vurobnukiv vapna)"
-    from_email = settings.EMAIL_HOST_USER
-    to_email = from_email
-    contact_message = "Контактна особа: %s, номер телефона: %s, вантаж: %s, код вантажу по залізниці: %s, станція навантаження: %s, код станції навантаження: %s, станція вивантаження: %s, код станція вивантаження: %s"%(contact_person, phone, cargo, cod_cargo_railway, load_station, cod_load_station, unloading_station, cod_unloading_station)
+        contact_message = "Фамілія: %s, імя: %s, почта: %s, номер телефона: %s, фракція: %s, підприємство: %s, період доставки: %s"%(user_surname, user_name, email, phone, select_fraction, enterprise, delivery_period)
     
-    if contact_person and phone and cargo and cod_cargo_railway and load_station and cod_load_station and unloading_station and cod_unloading_station:       
+
+    from_addr = 'antonsadlov@ukr.net'
+    to_addr = 'antonsadlov@ukr.net'
+
+    username = 'antonsadlov@ukr.net'
+    password = 'antoha2003'
+
+    msg = MIMEMultipart()
+
+    msg['From'] = from_addr
+    msg['To'] = to_addr
+    msg['Subject'] = 'Contact form(dla kombikormov i pidkormku ptizu)'
+    msg.attach(MIMEText(contact_message))
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.ukr.net:2525')
+    except TimeoutError:
+        return HttpResponseRedirect('/')
+
+    server.login(username,password)
+
+    if user_surname and volume and email and phone and select_fraction and enterprise and delivery_period:
         try:
-            send_mail(subject, contact_message, from_email, ['antonsadlov@gmail.com'])
+            server.sendmail(from_addr,to_addr,msg.as_string())
         except BadHeaderError:
             return HttpResponse('Invalid header found.')
+        server.quit()
+        return HttpResponseRedirect('../')
+    else:
+        return HttpResponse('Make sure all fields are entered and valid.')
+    
+
+
+def send_email_3_en(request):
+    user_surname = request.POST.get('user_surname', '')
+    volume = request.POST.get('volume', '')
+    email = request.POST.get('email', '')
+    phone = request.POST.get('phone', '')
+    select_fraction = request.POST.get('select_fraction', '')
+    enterprise = request.POST.get('enterprise', '')
+    delivery_period = request.POST.get('delivery_period', '')
+    message = request.POST['message']
+
+    
+    if message:
+        contact_message = "Контактна особа: %s, почта: %s, номер телефона: %s, обєм вантажу: %s, фракція: %s, підприємство: %s, період доставки: %s, текстов повідомлення: %s"%(user_surname, email, phone, volume, select_fraction, enterprise, delivery_period, message)
+    else:
+        contact_message = "Фамілія: %s, імя: %s, почта: %s, номер телефона: %s, фракція: %s, підприємство: %s, період доставки: %s"%(user_surname, user_name, email, phone, select_fraction, enterprise, delivery_period)
+    
+
+    from_addr = 'antonsadlov@ukr.net'
+    to_addr = 'antonsadlov@ukr.net'
+
+    username = 'antonsadlov@ukr.net'
+    password = 'antoha2003'
+
+    msg = MIMEMultipart()
+
+    msg['From'] = from_addr
+    msg['To'] = to_addr
+    msg['Subject'] = 'Contact form(dla zukrovoi promuslovosti)'
+    msg.attach(MIMEText(contact_message))
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.ukr.net:2525')
+    except TimeoutError:
+        return HttpResponseRedirect('/')
+
+    server.login(username,password)
+
+    if user_surname and volume and email and phone and select_fraction and enterprise and delivery_period:
+        try:
+            server.sendmail(from_addr,to_addr,msg.as_string())
+        except BadHeaderError:
+            return HttpResponse('Invalid header found.')
+        server.quit()
+        return HttpResponseRedirect('../')
+    else:
+        return HttpResponse('Make sure all fields are entered and valid.')
+    
+
+
+
+
+def send_email_2_en(request):
+    user_surname = request.POST.get('user_surname', '')
+    volume = request.POST.get('volume', '')
+    email = request.POST.get('email', '')
+    phone = request.POST.get('phone', '')
+    select_fraction = request.POST.get('select_fraction', '')
+    enterprise = request.POST.get('enterprise', '')
+    delivery_period = request.POST.get('delivery_period', '')
+    message = request.POST['message']
+
+    
+    if message:
+        contact_message = "Контактна особа: %s, почта: %s, номер телефона: %s, обєм вантажу: %s, фракція: %s, підприємство: %s, період доставки: %s, текстов повідомлення: %s"%(user_surname, email, phone, volume, select_fraction, enterprise, delivery_period, message)
+    else:
+        contact_message = "Фамілія: %s, імя: %s, почта: %s, номер телефона: %s, фракція: %s, підприємство: %s, період доставки: %s"%(user_surname, user_name, email, phone, select_fraction, enterprise, delivery_period)
+    
+
+    from_addr = 'antonsadlov@ukr.net'
+    to_addr = 'antonsadlov@ukr.net'
+
+    username = 'antonsadlov@ukr.net'
+    password = 'antoha2003'
+
+    msg = MIMEMultipart()
+
+    msg['From'] = from_addr
+    msg['To'] = to_addr
+    msg['Subject'] = 'Contact form(dla rozkusnenya gruntu i vodoyom)'
+    msg.attach(MIMEText(contact_message))
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.ukr.net:2525')
+    except TimeoutError:
+        return HttpResponseRedirect('/')
+
+    server.login(username,password)
+
+    if user_surname and volume and email and phone and select_fraction and enterprise and delivery_period:
+        try:
+            server.sendmail(from_addr,to_addr,msg.as_string())
+        except BadHeaderError:
+            return HttpResponse('Invalid header found.')
+        server.quit()
+        return HttpResponseRedirect('../')
+    else:
+        return HttpResponse('Make sure all fields are entered and valid.')
+
+    
+
+
+
+
+
+
+
+def send_email_1_en(request):
+    user_surname = request.POST.get('user_surname', '')
+    volume = request.POST.get('volume', '')
+    email = request.POST.get('email', '')
+    phone = request.POST.get('phone', '')
+    select_fraction = request.POST.get('select_fraction', '')
+    enterprise = request.POST.get('enterprise', '')
+    delivery_period = request.POST.get('delivery_period', '')
+    message = request.POST['message']
+
+    
+    if message:
+        contact_message = "Контактна особа: %s, почта: %s, номер телефона: %s, обєм вантажу: %s, фракція: %s, підприємство: %s, період доставки: %s, текстов повідомлення: %s"%(user_surname, email, phone, volume, select_fraction, enterprise, delivery_period, message)
+    else:
+        contact_message = "Фамілія: %s, імя: %s, почта: %s, номер телефона: %s, фракція: %s, підприємство: %s, період доставки: %s"%(user_surname, user_name, email, phone, select_fraction, enterprise, delivery_period)
+    
+
+    from_addr = 'antonsadlov@ukr.net'
+    to_addr = 'antonsadlov@ukr.net'
+
+    username = 'antonsadlov@ukr.net'
+    password = 'antoha2003'
+
+    msg = MIMEMultipart()
+
+    msg['From'] = from_addr
+    msg['To'] = to_addr
+    msg['Subject'] = 'Contact form(dla metalurgii ta vurobnukiv vapna)'
+    msg.attach(MIMEText(contact_message))
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.ukr.net:2525')
+    except TimeoutError:
+        return HttpResponseRedirect('/')
+
+    server.login(username,password)
+
+    if user_surname and volume and email and phone and select_fraction and enterprise and delivery_period:
+        try:
+            server.sendmail(from_addr,to_addr,msg.as_string())
+        except BadHeaderError:
+            return HttpResponse('Invalid header found.')
+        server.quit()
         return HttpResponseRedirect('../')
     else:
         return HttpResponse('Make sure all fields are entered and valid.')
