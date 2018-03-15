1f77000835c7d10c086bf2fdad08c08a4647c0f4
anton3808 <antonsadlov@gmail.com>
2018-03-15 23:12:59 +0200

    Add files via upload

--- production_en/views.py
+++ production_en/views.py
@@ -17,22 +17,100 @@ def send_email_4(request):
     phone = request.POST.get('phone', '')
     select_fraction = request.POST.get('select_fraction', '')
     enterprise = request.POST.get('enterprise', '')
-    delivery_period = request.POST.get('delivery_period', '')
+    delivery_period_1 = request.POST.get('delivery_period_1', '')
+    delivery_period_2 = request.POST.get('delivery_period_2', '')
     message = request.POST['message']
+
+
+
+    if select_fraction == "1":
+        select_fraction = "0-5 мм"
+    elif select_fraction == "2":
+        select_fraction = "0-20 мм"
+    elif select_fraction == "3":
+        select_fraction = "20-40 мм"
+    elif select_fraction == "4":
+        select_fraction = "40-80 мм"
+    elif select_fraction == "5":
+        select_fraction = "80-120мм"
+
+
+
+
+    if delivery_period_1 == "1":
+        delivery_period_1 = "січеня"
+    elif delivery_period_1 == "2":
+        delivery_period_1 = "лютого"
+    elif delivery_period_1 == "3":
+        delivery_period_1 = "березня"
+    elif delivery_period_1 == "4":
+        delivery_period_1 = "квітня"
+    elif delivery_period_1 == "5":
+        delivery_period_1 = "травня"
+    elif delivery_period_1 == "6":
+        delivery_period_1 = "червня"
+    elif delivery_period_1 == "7":
+        delivery_period_1 = "липня"
+    elif delivery_period_1 == "8":
+        delivery_period_1 = "серпня"
+    elif delivery_period_1 == "9":
+        delivery_period_1 = "вересня"
+    elif delivery_period_1 == "10":
+        delivery_period_1 = "жовтня"
+    elif delivery_period_1 == "11":
+        delivery_period_1 = "листопада"
+    elif delivery_period_1 == "12":
+        delivery_period_1 = "грудня"
+
+
+
+
+
+    if delivery_period_2 == "1":
+        delivery_period_2 = "січень"
+    elif delivery_period_2 == "2":
+        delivery_period_2 = "лютий"
+    elif delivery_period_2 == "3":
+        delivery_period_2 = "березень"
+    elif delivery_period_2 == "4":
+        delivery_period_2 = "квітень"
+    elif delivery_period_2 == "5":
+        delivery_period_2 = "травень"
+    elif delivery_period_2 == "6":
+        delivery_period_2 = "червень"
+    elif delivery_period_2 == "7":
+        delivery_period_2 = "липень"
+    elif delivery_period_2 == "8":
+        delivery_period_2 = "серпень"
+    elif delivery_period_2 == "9":
+        delivery_period_2 = "вересень"
+    elif delivery_period_2 == "10":
+        delivery_period_2 = "жовтень"
+    elif delivery_period_2 == "11":
+        delivery_period_2 = "листопад"
+    elif delivery_period_2 == "12":
+        delivery_period_2 = "грудень"
 
     
     if message:
-        contact_message = "Контактна особа: %s, почта: %s, номер телефона: %s, обєм вантажу: %s, фракція: %s, підприємство: %s, період доставки: %s, текстов повідомлення: %s"%(user_surname, email, phone, volume, select_fraction, enterprise, delivery_period, message)
+        contact_message = "Контактна особа: %s, почта: %s, номер телефона: %s, обєм вантажу: %s, фракція: %s, підприємство: %s, період доставки: з %s по %s, текстов повідомлення: %s"%(user_surname, email, phone, volume, select_fraction, enterprise, delivery_period_1, delivery_period_2, message)
     else:
-        contact_message = "Фамілія: %s, імя: %s, почта: %s, номер телефона: %s, фракція: %s, підприємство: %s, період доставки: %s"%(user_surname, user_name, email, phone, select_fraction, enterprise, delivery_period)
+        contact_message = "Фамілія: %s, імя: %s, почта: %s, номер телефона: %s, фракція: %s, підприємство: %s, період доставки: з %s по %s"%(user_surname, user_name, email, phone, select_fraction, enterprise, delivery_period_1, delivery_period_2)
     
 
-    from_addr = 'tpk_triumph@ukr.net'
-    to_addr = 'tpk_triumph@ukr.net'
+    # from_addr = 'tpk_triumph@ukr.net'
+    # to_addr = 'tpk_triumph@ukr.net'
 
-    username = 'tpk_triumph@ukr.net'
-    password = 'Sad_!123'
+    # username = 'tpk_triumph@ukr.net'
+    # password = 'Sad_!123'
 
+
+    from_addr = 'antonsadlov@ukr.net'
+    to_addr = 'antonsadlov@ukr.net'
+
+    username = 'antonsadlov@ukr.net'
+    password = 'antoha2003'
+    
     msg = MIMEMultipart()
 
     msg['From'] = from_addr
@@ -47,7 +125,7 @@ def send_email_4(request):
 
     server.login(username,password)
 
-    if user_surname and volume and email and phone and select_fraction and enterprise and delivery_period:
+    if user_surname and volume and email and phone and select_fraction and enterprise and delivery_period_1 and delivery_period_2:
         try:
             server.sendmail(from_addr,to_addr,msg.as_string())
         except BadHeaderError:
@@ -67,21 +145,93 @@ def send_email_3(request):
     phone = request.POST.get('phone', '')
     select_fraction = request.POST.get('select_fraction', '')
     enterprise = request.POST.get('enterprise', '')
-    delivery_period = request.POST.get('delivery_period', '')
+    delivery_period_1 = request.POST.get('delivery_period_1', '')
+    delivery_period_2 = request.POST.get('delivery_period_2', '')
     message = request.POST['message']
+
+
+
+    if select_fraction == "1":
+        select_fraction = "40-80 мм"
+    elif select_fraction == "2":
+        select_fraction = "80-120мм"
+
+
+
+
+    if delivery_period_1 == "1":
+        delivery_period_1 = "січеня"
+    elif delivery_period_1 == "2":
+        delivery_period_1 = "лютого"
+    elif delivery_period_1 == "3":
+        delivery_period_1 = "березня"
+    elif delivery_period_1 == "4":
+        delivery_period_1 = "квітня"
+    elif delivery_period_1 == "5":
+        delivery_period_1 = "травня"
+    elif delivery_period_1 == "6":
+        delivery_period_1 = "червня"
+    elif delivery_period_1 == "7":
+        delivery_period_1 = "липня"
+    elif delivery_period_1 == "8":
+        delivery_period_1 = "серпня"
+    elif delivery_period_1 == "9":
+        delivery_period_1 = "вересня"
+    elif delivery_period_1 == "10":
+        delivery_period_1 = "жовтня"
+    elif delivery_period_1 == "11":
+        delivery_period_1 = "листопада"
+    elif delivery_period_1 == "12":
+        delivery_period_1 = "грудня"
+
+
+
+
+
+    if delivery_period_2 == "1":
+        delivery_period_2 = "січень"
+    elif delivery_period_2 == "2":
+        delivery_period_2 = "лютий"
+    elif delivery_period_2 == "3":
+        delivery_period_2 = "березень"
+    elif delivery_period_2 == "4":
+        delivery_period_2 = "квітень"
+    elif delivery_period_2 == "5":
+        delivery_period_2 = "травень"
+    elif delivery_period_2 == "6":
+        delivery_period_2 = "червень"
+    elif delivery_period_2 == "7":
+        delivery_period_2 = "липень"
+    elif delivery_period_2 == "8":
+        delivery_period_2 = "серпень"
+    elif delivery_period_2 == "9":
+        delivery_period_2 = "вересень"
+    elif delivery_period_2 == "10":
+        delivery_period_2 = "жовтень"
+    elif delivery_period_2 == "11":
+        delivery_period_2 = "листопад"
+    elif delivery_period_2 == "12":
+        delivery_period_2 = "грудень"
 
     
     if message:
-        contact_message = "Контактна особа: %s, почта: %s, номер телефона: %s, обєм вантажу: %s, фракція: %s, підприємство: %s, період доставки: %s, текстов повідомлення: %s"%(user_surname, email, phone, volume, select_fraction, enterprise, delivery_period, message)
+        contact_message = "Контактна особа: %s, почта: %s, номер телефона: %s, обєм вантажу: %s, фракція: %s, підприємство: %s, період доставки: з %s по %s, текстов повідомлення: %s"%(user_surname, email, phone, volume, select_fraction, enterprise, delivery_period_1, delivery_period_2, message)
     else:
-        contact_message = "Фамілія: %s, імя: %s, почта: %s, номер телефона: %s, фракція: %s, підприємство: %s, період доставки: %s"%(user_surname, user_name, email, phone, select_fraction, enterprise, delivery_period)
+        contact_message = "Фамілія: %s, імя: %s, почта: %s, номер телефона: %s, фракція: %s, підприємство: %s, період доставки: з %s по %s"%(user_surname, user_name, email, phone, select_fraction, enterprise, delivery_period_1, delivery_period_2)
     
 
-    from_addr = 'tpk_triumph@ukr.net'
-    to_addr = 'tpk_triumph@ukr.net'
+    # from_addr = 'tpk_triumph@ukr.net'
+    # to_addr = 'tpk_triumph@ukr.net'
 
-    username = 'tpk_triumph@ukr.net'
-    password = 'Sad_!123'
+    # username = 'tpk_triumph@ukr.net'
+    # password = 'Sad_!123'
+
+
+    from_addr = 'antonsadlov@ukr.net'
+    to_addr = 'antonsadlov@ukr.net'
+
+    username = 'antonsadlov@ukr.net'
+    password = 'antoha2003'
 
     msg = MIMEMultipart()
 
@@ -97,7 +247,7 @@ def send_email_3(request):
 
     server.login(username,password)
 
-    if user_surname and volume and email and phone and select_fraction and enterprise and delivery_period:
+    if user_surname and volume and email and phone and select_fraction and enterprise and delivery_period_1 and delivery_period_2:
         try:
             server.sendmail(from_addr,to_addr,msg.as_string())
         except BadHeaderError:
@@ -119,21 +269,97 @@ def send_email_2(request):
     phone = request.POST.get('phone', '')
     select_fraction = request.POST.get('select_fraction', '')
     enterprise = request.POST.get('enterprise', '')
-    delivery_period = request.POST.get('delivery_period', '')
+    delivery_period_1 = request.POST.get('delivery_period_1', '')
+    delivery_period_2 = request.POST.get('delivery_period_2', '')
     message = request.POST['message']
+
+
+
+    if select_fraction == "1":
+        select_fraction = "0-1 мм"
+    elif select_fraction == "2":
+        select_fraction = "0-5 мм"
+    elif select_fraction == "3":
+        select_fraction = "0-20 мм"
+    elif select_fraction == "4":
+        select_fraction = "20-40 мм"
+
+
+
+
+    if delivery_period_1 == "1":
+        delivery_period_1 = "січеня"
+    elif delivery_period_1 == "2":
+        delivery_period_1 = "лютого"
+    elif delivery_period_1 == "3":
+        delivery_period_1 = "березня"
+    elif delivery_period_1 == "4":
+        delivery_period_1 = "квітня"
+    elif delivery_period_1 == "5":
+        delivery_period_1 = "травня"
+    elif delivery_period_1 == "6":
+        delivery_period_1 = "червня"
+    elif delivery_period_1 == "7":
+        delivery_period_1 = "липня"
+    elif delivery_period_1 == "8":
+        delivery_period_1 = "серпня"
+    elif delivery_period_1 == "9":
+        delivery_period_1 = "вересня"
+    elif delivery_period_1 == "10":
+        delivery_period_1 = "жовтня"
+    elif delivery_period_1 == "11":
+        delivery_period_1 = "листопада"
+    elif delivery_period_1 == "12":
+        delivery_period_1 = "грудня"
+
+
+
+
+
+    if delivery_period_2 == "1":
+        delivery_period_2 = "січень"
+    elif delivery_period_2 == "2":
+        delivery_period_2 = "лютий"
+    elif delivery_period_2 == "3":
+        delivery_period_2 = "березень"
+    elif delivery_period_2 == "4":
+        delivery_period_2 = "квітень"
+    elif delivery_period_2 == "5":
+        delivery_period_2 = "травень"
+    elif delivery_period_2 == "6":
+        delivery_period_2 = "червень"
+    elif delivery_period_2 == "7":
+        delivery_period_2 = "липень"
+    elif delivery_period_2 == "8":
+        delivery_period_2 = "серпень"
+    elif delivery_period_2 == "9":
+        delivery_period_2 = "вересень"
+    elif delivery_period_2 == "10":
+        delivery_period_2 = "жовтень"
+    elif delivery_period_2 == "11":
+        delivery_period_2 = "листопад"
+    elif delivery_period_2 == "12":
+        delivery_period_2 = "грудень"
 
     
     if message:
-        contact_message = "Контактна особа: %s, почта: %s, номер телефона: %s, обєм вантажу: %s, фракція: %s, підприємство: %s, період доставки: %s, текстов повідомлення: %s"%(user_surname, email, phone, volume, select_fraction, enterprise, delivery_period, message)
+        contact_message = "Контактна особа: %s, почта: %s, номер телефона: %s, обєм вантажу: %s, фракція: %s, підприємство: %s, період доставки: з %s по %s, текстов повідомлення: %s"%(user_surname, email, phone, volume, select_fraction, enterprise, delivery_period_1, delivery_period_2, message)
     else:
-        contact_message = "Фамілія: %s, імя: %s, почта: %s, номер телефона: %s, фракція: %s, підприємство: %s, період доставки: %s"%(user_surname, user_name, email, phone, select_fraction, enterprise, delivery_period)
+        contact_message = "Фамілія: %s, імя: %s, почта: %s, номер телефона: %s, фракція: %s, підприємство: %s, період доставки: з %s по %s"%(user_surname, user_name, email, phone, select_fraction, enterprise, delivery_period_1, delivery_period_2)
     
 
-    from_addr = 'tpk_triumph@ukr.net'
-    to_addr = 'tpk_triumph@ukr.net'
+    # from_addr = 'tpk_triumph@ukr.net'
+    # to_addr = 'tpk_triumph@ukr.net'
 
-    username = 'tpk_triumph@ukr.net'
-    password = 'Sad_!123'
+    # username = 'tpk_triumph@ukr.net'
+    # password = 'Sad_!123'
+
+
+    from_addr = 'antonsadlov@ukr.net'
+    to_addr = 'antonsadlov@ukr.net'
+
+    username = 'antonsadlov@ukr.net'
+    password = 'antoha2003'
 
     msg = MIMEMultipart()
 
@@ -149,7 +375,7 @@ def send_email_2(request):
 
     server.login(username,password)
 
-    if user_surname and volume and email and phone and select_fraction and enterprise and delivery_period:
+    if user_surname and volume and email and phone and select_fraction and enterprise and delivery_period_1 and delivery_period_2:
         try:
             server.sendmail(from_addr,to_addr,msg.as_string())
         except BadHeaderError:
@@ -175,21 +401,99 @@ def send_email_1(request):
     phone = request.POST.get('phone', '')
     select_fraction = request.POST.get('select_fraction', '')
     enterprise = request.POST.get('enterprise', '')
-    delivery_period = request.POST.get('delivery_period', '')
+    delivery_period_1 = request.POST.get('delivery_period_1', '')
+    delivery_period_2 = request.POST.get('delivery_period_2', '')
     message = request.POST['message']
+
+
+
+    if select_fraction == "1":
+        select_fraction = "0-5 мм"
+    elif select_fraction == "2":
+        select_fraction = "0-20 мм"
+    elif select_fraction == "3":
+        select_fraction = "20-40 мм"
+    elif select_fraction == "4":
+        select_fraction = "40-80 мм"
+    elif select_fraction == "5":
+        select_fraction = "80-120мм"
+
+
+
+
+    if delivery_period_1 == "1":
+        delivery_period_1 = "січеня"
+    elif delivery_period_1 == "2":
+        delivery_period_1 = "лютого"
+    elif delivery_period_1 == "3":
+        delivery_period_1 = "березня"
+    elif delivery_period_1 == "4":
+        delivery_period_1 = "квітня"
+    elif delivery_period_1 == "5":
+        delivery_period_1 = "травня"
+    elif delivery_period_1 == "6":
+        delivery_period_1 = "червня"
+    elif delivery_period_1 == "7":
+        delivery_period_1 = "липня"
+    elif delivery_period_1 == "8":
+        delivery_period_1 = "серпня"
+    elif delivery_period_1 == "9":
+        delivery_period_1 = "вересня"
+    elif delivery_period_1 == "10":
+        delivery_period_1 = "жовтня"
+    elif delivery_period_1 == "11":
+        delivery_period_1 = "листопада"
+    elif delivery_period_1 == "12":
+        delivery_period_1 = "грудня"
+
+
+
+
+
+    if delivery_period_2 == "1":
+        delivery_period_2 = "січень"
+    elif delivery_period_2 == "2":
+        delivery_period_2 = "лютий"
+    elif delivery_period_2 == "3":
+        delivery_period_2 = "березень"
+    elif delivery_period_2 == "4":
+        delivery_period_2 = "квітень"
+    elif delivery_period_2 == "5":
+        delivery_period_2 = "травень"
+    elif delivery_period_2 == "6":
+        delivery_period_2 = "червень"
+    elif delivery_period_2 == "7":
+        delivery_period_2 = "липень"
+    elif delivery_period_2 == "8":
+        delivery_period_2 = "серпень"
+    elif delivery_period_2 == "9":
+        delivery_period_2 = "вересень"
+    elif delivery_period_2 == "10":
+        delivery_period_2 = "жовтень"
+    elif delivery_period_2 == "11":
+        delivery_period_2 = "листопад"
+    elif delivery_period_2 == "12":
+        delivery_period_2 = "грудень"
 
     
     if message:
-        contact_message = "Контактна особа: %s, почта: %s, номер телефона: %s, обєм вантажу: %s, фракція: %s, підприємство: %s, період доставки: %s, текстов повідомлення: %s"%(user_surname, email, phone, volume, select_fraction, enterprise, delivery_period, message)
+        contact_message = "Контактна особа: %s, почта: %s, номер телефона: %s, обєм вантажу: %s, фракція: %s, підприємство: %s, період доставки: з %s по %s, текстов повідомлення: %s"%(user_surname, email, phone, volume, select_fraction, enterprise, delivery_period_1, delivery_period_2, message)
     else:
-        contact_message = "Фамілія: %s, імя: %s, почта: %s, номер телефона: %s, фракція: %s, підприємство: %s, період доставки: %s"%(user_surname, user_name, email, phone, select_fraction, enterprise, delivery_period)
+        contact_message = "Фамілія: %s, імя: %s, почта: %s, номер телефона: %s, фракція: %s, підприємство: %s, період доставки: з %s по %s"%(user_surname, user_name, email, phone, select_fraction, enterprise, delivery_period_1, delivery_period_2)
     
 
-    from_addr = 'tpk_triumph@ukr.net'
-    to_addr = 'tpk_triumph@ukr.net'
+    # from_addr = 'tpk_triumph@ukr.net'
+    # to_addr = 'tpk_triumph@ukr.net'
 
-    username = 'tpk_triumph@ukr.net'
-    password = 'Sad_!123'
+    # username = 'tpk_triumph@ukr.net'
+    # password = 'Sad_!123'
+
+
+    from_addr = 'antonsadlov@ukr.net'
+    to_addr = 'antonsadlov@ukr.net'
+
+    username = 'antonsadlov@ukr.net'
+    password = 'antoha2003'
 
     msg = MIMEMultipart()
 
@@ -205,7 +509,7 @@ def send_email_1(request):
 
     server.login(username,password)
 
-    if user_surname and volume and email and phone and select_fraction and enterprise and delivery_period:
+    if user_surname and volume and email and phone and select_fraction and enterprise and delivery_period_1 and delivery_period_2:
         try:
             server.sendmail(from_addr,to_addr,msg.as_string())
         except BadHeaderError:
